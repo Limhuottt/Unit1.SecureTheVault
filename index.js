199ee8d3c50235fb91f9cd32cf35dd7d157502a9
Limhuot Sung <Limhuot_sung@yahoo.com>
2025-05-05 14:04:30 -0700

    .\index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = (100 - 20) / 2;
+
+let code3 = 6 * 7 + 3;
+
+// Output the combination to the console
+console.log("Vault Combination:");
+console.log("Code 1:", code1);
+console.log("Code 2:", code2);
+console.log("Code 3:", code3);
+
