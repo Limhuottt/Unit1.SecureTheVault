eafebb2a7d3ac6b552e33e2354d65e1d709b16c6
Limhuot Sung <Limhuot_sung@yahoo.com>
2025-05-05 14:18:28 -0700

    .\index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-let code1;
+var code1;
 
 code1 = 7 + 3;
 
-let code2 = (100 - 20) / 2;
+var code2 = (100 - 20) / 2;
 
-let code3 = 6 * 7 + 3;
+var code3 = 6 * 7 + 3;
+
+var message = "The vault has been sescured. The combination is:";
+
+var codeA = code1 + "-" + code2 + "-" + code3;
+
+var codeb = `${code1}-${code2}-${code3}`;
 
 // Output the combination to the console
 console.log("Vault Combination:");
 console.log("Code 1:", code1);
 console.log("Code 2:", code2);
 console.log("Code 3:", code3);
-
